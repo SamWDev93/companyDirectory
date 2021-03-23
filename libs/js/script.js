07b68ac6c7d72f8c846435ee32a529229f4bf905
Sam Welsh <samwelsh@hotmail.co.uk>
2021-03-23 09:44:26 +0000

    Add for loop create card for each result

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,9 +1,3 @@
-function createCard(result) {
-  var card =
-    "<div class='card'><img src='./libs/images/user-icon.png' ><div class='card-body'><p class='fullName'></p><p class='jobTitle'></p><p class='department'></p><p class='location'></p><p class='email'></p><button type='button' class='btn btn-primary btn-sm'>Edit</button><button type='button' class='btn btn-danger btn-sm'>Delete</button></div></div>";
-  return card;
-}
-
 $(document).ready(() => {
   $.ajax({
     url: "libs/php/getAll.php",
@@ -11,15 +5,14 @@ $(document).ready(() => {
     dataType: "json",
 
     success: function (result) {
-      console.log(result);
-      result.data.forEach((record) => {
-        $(".fullName").html(record.firstName + " " + record.lastName);
-        $(".jobTitle").html(record.jobTitle);
-        $(".department").html(record.department);
-        $(".location").html(record.location);
-        $(".email").html(record.email);
-        $(".records").append(createCard(record));
-      });
+      if (result.status.name == "ok") {
+        console.log(result);
+        for (let i = 0; i < result.data.length; i++) {
+          $(".records").append(
+            `<div class='card'><img src='./libs/images/user-icon.png' ><div class='card-body'><p class='fullName'>${result.data[i].firstName} ${result.data[i].lastName}</p><p class='jobTitle'>${result.data[i].jobTitle}</p><p class='department'>${result.data[i].department}</p><p class='location'>${result.data[i].location}</p><p class='email'>${result.data[i].email}</p><button type='button' class='btn btn-primary btn-sm'>Edit</button><button type='button' class='btn btn-danger btn-sm'>Delete</button></div></div>`
+          );
+        }
+      }
     },
 
     error: function (jqXHR, textStatus, errorThrown) {
