42139d80b01717b7167743fc25a9cc953587696a
Sam Welsh <samwelsh@hotmail.co.uk>
2021-03-23 08:48:25 +0000

    Add edit and delete buttons to cards

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,6 +1,6 @@
 function createCard(result) {
   var card =
-    "<div class='card'><img src='./libs/images/user-icon.png' ><div class='card-body'><p class='fullName'></p><p class='jobTitle'></p><p class='department'></p><p class='location'></p><p class='email'></p></div></div>";
+    "<div class='card'><img src='./libs/images/user-icon.png' ><div class='card-body'><p class='fullName'></p><p class='jobTitle'></p><p class='department'></p><p class='location'></p><p class='email'></p><button type='button' class='btn btn-primary btn-sm'>Edit</button><button type='button' class='btn btn-danger btn-sm'>Delete</button></div></div>";
   return card;
 }
 
