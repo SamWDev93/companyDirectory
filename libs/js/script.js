c670cf46e0e465a2e519b0456ac0b4e6b58a2298
Sam Welsh <samwelsh@hotmail.co.uk>
2021-03-25 17:01:33 +0000

    Add filter by department routine and function

--- libs/js/script.js
+++ libs/js/script.js
@@ -103,6 +103,44 @@ function getAllLocations() {
   });
 }
 
+// Filter functions
+function employeeFilterByDepartment() {
+  $.ajax({
+    url: "libs/php/employeeFilterByDepartment.php",
+    type: "POST",
+    dataType: "json",
+    data: {
+      id: $("#filterByDepartment").val(),
+    },
+
+    success: function (result) {
+      if (result.status.name == "ok") {
+        console.log(result);
+        totalRecords = 0;
+
+        $(".records").empty();
+        for (let i = 0; i < result.data.length; i++) {
+          totalRecords++;
+        }
+
+        $(".records").append(
+          `<div class='totalRecords container-fluid'><h5>Displaying ${totalRecords} Employee Records:</h5></div>`
+        );
+
+        for (let i = 0; i < result.data.length; i++) {
+          $(".records").append(
+            `<div class='card'><table><tr><td class='alignCenter'><img src='./libs/images/user-icon.png'></td></tr></table><div class='card-body'><table><tr><td class='fullName alignCenter'><b>${result.data[i].firstName} ${result.data[i].lastName}</b><a href='mailto:${result.data[i].email}'><i class="fas fa-envelope-open-text"></i></a></td></tr></table><table class='table table-striped mt-4'><tr><td class='alignLeft'><i class="fas fa-network-wired"></i><b>Department: </b></td><td class='department alignRight'>${result.data[i].department}</td></tr><tr><td class='alignLeft'><i class="fas fa-search-location"></i><b>Location:</b></td><td class='location alignRight'>${result.data[i].location}</td></tr></table><table class='mt-5'><tr><td class='alignCenter'><button type='button' class='btn btn-primary btn-sm'>Edit</button></td><td class='alignCenter'><button type='button' class='btn btn-danger btn-sm'>Delete</button></td></tr></table></div></div>`
+          );
+        }
+      }
+    },
+
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+    },
+  });
+}
+
 // Retrieve all records on load
 $(document).ready(() => {
   getAllEmployees();
@@ -161,5 +199,13 @@ $("#searchFor").change(function () {
 
   if ($("#searchFor").val() == "locations") {
     getAllLocations();
+  }
+});
+
+$("#filterByDepartment").change(function () {
+  if ($("#filterByDepartment").val() == "allDepartments") {
+    getAllEmployees();
+  } else {
+    employeeFilterByDepartment();
   }
 });
