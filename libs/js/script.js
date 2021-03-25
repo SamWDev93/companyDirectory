174d9ef358910c7405dd3612c74a899447777782
Sam Welsh <samwelsh@hotmail.co.uk>
2021-03-25 11:10:25 +0000

    Add and define functions for search routines

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,5 +1,4 @@
-// Retrieve all records on load
-$(document).ready(() => {
+function getAllEmployees() {
   $.ajax({
     url: "libs/php/getAll.php",
     type: "GET",
@@ -8,6 +7,7 @@ $(document).ready(() => {
     success: function (result) {
       if (result.status.name == "ok") {
         console.log(result);
+        $(".records").empty();
         for (let i = 0; i < result.data.length; i++) {
           $(".records").append(
             `<div class='card'><table><tr><td class='alignCenter'><img src='./libs/images/user-icon.png'></td></tr></table><div class='card-body'><table><tr><td class='fullName alignCenter'><b>${result.data[i].firstName} ${result.data[i].lastName}</b><a href='mailto:${result.data[i].email}'><i class="fas fa-envelope-open-text"></i></a></td></tr></table><table class='table table-striped mt-4'><tr><td class='alignLeft'><i class="fas fa-network-wired"></i><b>Department: </b></td><td class='department alignRight'>${result.data[i].department}</td></tr><tr><td class='alignLeft'><i class="fas fa-search-location"></i><b>Location:</b></td><td class='location alignRight'>${result.data[i].location}</td></tr></table><table class='mt-5'><tr><td class='alignCenter'><button type='button' class='btn btn-primary btn-sm'>Edit</button></td><td class='alignCenter'><button type='button' class='btn btn-danger btn-sm'>Delete</button></td></tr></table></div></div>`
@@ -20,6 +20,59 @@ $(document).ready(() => {
       console.log(errorThrown);
     },
   });
+}
+
+function getAllDepartments() {
+  $.ajax({
+    url: "libs/php/getAllDepartments.php",
+    type: "GET",
+    dataType: "json",
+
+    success: function (result) {
+      if (result.status.name == "ok") {
+        console.log(result);
+        $(".records").empty();
+        for (let i = 0; i < result.data.length; i++) {
+          $(".records").append(
+            `<div class='card'><table><tr><td class='departmentIcon alignCenter'><img src='./libs/images/department-icon.png'></td></tr></table><div class='card-body'><table><tr><td class='departmentName alignCenter'><b>${result.data[i].name}</b></td></tr></table><table class='mt-5'><tr><td class='alignCenter'><button type='button' class='btn btn-primary btn-sm'>Edit</button></td><td class='alignCenter'><button type='button' class='btn btn-danger btn-sm'>Delete</button></td></tr></table></div></div>`
+          );
+        }
+      }
+    },
+
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+    },
+  });
+}
+
+function getAllLocations() {
+  $.ajax({
+    url: "libs/php/getAllLocations.php",
+    type: "GET",
+    dataType: "json",
+
+    success: function (result) {
+      if (result.status.name == "ok") {
+        console.log(result);
+        $(".records").empty();
+        for (let i = 0; i < result.data.length; i++) {
+          $(".records").append(
+            `<div class='card'><table><tr><td class='locationIcon alignCenter'><img src='./libs/images/location-icon.png'></td></tr></table><div class='card-body'><table><tr><td class='departmentName alignCenter'><b>${result.data[i].name}</b></td></tr></table><table class='mt-5'><tr><td class='alignCenter'><button type='button' class='btn btn-primary btn-sm'>Edit</button></td><td class='alignCenter'><button type='button' class='btn btn-danger btn-sm'>Delete</button></td></tr></table></div></div>`
+          );
+        }
+      }
+    },
+
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(errorThrown);
+    },
+  });
+}
+
+// Retrieve all records on load
+$(document).ready(() => {
+  getAllEmployees();
 });
 
 // Dynamically populate department select filter
@@ -62,4 +115,18 @@ $.ajax({
   error: function (jqXHR, textStatus, errorThrown) {
     console.log(errorThrown);
   },
+});
+
+$("#searchFor").change(function () {
+  if ($("#searchFor").val() == "employees") {
+    getAllEmployees();
+  }
+
+  if ($("#searchFor").val() == "departments") {
+    getAllDepartments();
+  }
+
+  if ($("#searchFor").val() == "locations") {
+    getAllLocations();
+  }
 });
